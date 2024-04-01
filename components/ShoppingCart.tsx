fed36e1c4375e103078d820fc1ffce86f8c0becb
Rohitkumar1212 <1997errohitkumar@gmail.com>
2024-04-01 20:44:52 +0530

    added increment and decrement of the quantity

--- components/ShoppingCart.tsx
+++ components/ShoppingCart.tsx
@@ -1,12 +1,13 @@
 "use client"
 import { useAppDispatch, useAppSelector } from '@/lib/supabase/hooks/redux'
-import { getCart, removeFromTheCart } from '@/redux/cartSlice'
+import { decreamentQuantity, getCart, increamentQuantity, removeFromTheCart } from '@/redux/cartSlice'
 import Image from 'next/image'
 import React from 'react'
 
 const ShoppingCart = () => {
     const cart = useAppSelector(getCart)
     const dispatch = useAppDispatch()
+    // console.log("cart", cart)
     return (
         <div>
             <div className='flex justify-between items-center  border-b border-gray-300 py-5 '>
@@ -15,10 +16,10 @@ const ShoppingCart = () => {
             </div>
             {
                 cart.map((product: any) => (
-                    <div className='mt-4 flex justify-between'>
+                    <div key={product.id} className='mt-4 flex justify-between'>
                         <div className='flex'>
                             <div >
-                                <Image src={product.image} alt={product.title} height={100} width={100} />
+                                <Image src={product.image} alt={product.title ? product.title : "image"} height={100} width={100} />
                             </div>
                             <div className='ml-4'>
                                 <h2 className='font-medium'>{product.title}</h2>
@@ -27,9 +28,13 @@ const ShoppingCart = () => {
                                 onClick={()=>dispatch(removeFromTheCart(product.id))}
                                 className='font-bold cursor-pointer text-red-600'>Remove</h3>
                                 <div className='text-xl font-medium flex justify-between items-center w-fit bg-gray-200 rounded-md px-5 py-1 my-4'>
-                                    <div className='cursor-pointer mr-4'>-</div>
-                                    <div className='cursor-pointer'>0</div>
-                                    <div className='cursor-pointer ml-4'>+</div>
+                                    <div
+                                    onClick={()=>dispatch(decreamentQuantity(product))}
+                                    className='cursor-pointer mr-4'>-</div>
+                                    <div className='cursor-pointer'>{product.quantity}</div>
+                                    <div
+                                    onClick={()=>dispatch(increamentQuantity(product))}
+                                    className='cursor-pointer ml-4'>+</div>
                                 </div>
                             </div>
                         </div>
--- redux/cartSlice.ts
+++ redux/cartSlice.ts
@@ -16,14 +16,43 @@ const cartSlice = createSlice({
     reducers:{
         //multiple actions
         addToCart: (state, action)=>{
-            state.cart.push(action.payload)
+            const isPresent  = state.cart.find((item:any)=>{
+                return item.id === action.payload.id
+            })
+            if(isPresent){
+                //update quantity only
+                state.cart = state.cart.map((item:any)=>{
+                    return item.id === action.payload.id ?({...item, quantity: item.quantity+1}):item;
+                })
+            }else{
+                //add the item in the array
+                state.cart.push({...action.payload, quantity: 1})
+            }
+            
         },
         removeFromTheCart:(state, action)=>{
             state.cart = state.cart.filter((item:any )=>item.id !== action.payload)
         },
+        increamentQuantity:(state, action)=>{
+            state.cart = state.cart.map((item:any)=>{
+                return item.id === action.payload.id ?({...item, quantity: item.quantity+1}):item;
+            })
+        },
+        decreamentQuantity:(state, action)=>{
+            state.cart = state.cart.map((item:any)=>{
+                // return item.id === action.payload.id ?({...item, quantity: item.quantity-1}):item;
+                //handling non negative item value
+                if (item.id === action.payload.id) {
+                    const newQuantity = Math.max(item.quantity - 1, 0); // Ensures quantity won't be negative
+                    return { ...item, quantity: newQuantity };
+                } else {
+                    return item;
+                }
+            })
+        }
     }
 })
-export const { addToCart, removeFromTheCart } = cartSlice.actions;
+export const { addToCart, removeFromTheCart, increamentQuantity, decreamentQuantity } = cartSlice.actions;
 
 export const getCart = (state: RootState)=>state.cart.cart
 export default cartSlice.reducer
