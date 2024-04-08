3fc8e5bd4c9ce30ab319c54f6c0219e22b1afd79
Rohitkumar1212 <1997errohitkumar@gmail.com>
2024-04-09 00:09:02 +0530

    es lint bugged fixed in the product/[id] page - stable

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -3,14 +3,14 @@ import React, { useEffect } from 'react'
 import SingleProduct from "@/components/SingleProduct"
 import { useParams } from 'next/navigation'
 import { useSupabase } from '@/lib/supabase/hooks/useSupabase'
-const page = () => {
+const ProductPage = () => {
     const { id } = useParams()
     console.log("typeof id", typeof(id))
     const { singleProduct, getSingleProductFromSupabase } = useSupabase()
 
     useEffect(() => {
         getSingleProductFromSupabase(Number(id))
-    }, [])
+    }, [id, getSingleProductFromSupabase])
     return (
         <div className='flex'>
             <SingleProduct singleProduct={singleProduct}/>
@@ -19,4 +19,4 @@ const page = () => {
     )
 }
 
-export default page
+export default ProductPage
